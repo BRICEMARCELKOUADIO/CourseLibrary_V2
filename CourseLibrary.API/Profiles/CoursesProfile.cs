7babce8141772f20eedb339790497dd5288d59b1
Brice Marcel <kbricemarcel@outlook.fr>
2021-05-13 17:26:33 +0000

    Correction

--- CourseLibrary.API/Profiles/CoursesProfile.cs
+++ CourseLibrary.API/Profiles/CoursesProfile.cs
@@ -12,8 +12,8 @@ namespace CourseLibrary.API.Profiles
         {
             CreateMap<Entities.Course, Models.CourseDto>();
             CreateMap<Models.CourseForCreationDto, Entities.Course>();
-            CreateMap<Models.CourseForUpdateDto, Entities.Course>();
-            CreateMap<Entities.Course, Models.CourseForUpdateDto>();
+            CreateMap<Models.CourseForUpdateDto, Entities.Course>().ReverseMap();
+            //CreateMap<Entities.Course, Models.CourseForUpdateDto>();
         }
     }
 }
